8eb13a736c6c4167f70fbb4438e46d0f488ea159
Praveen Perera <praveen@avencera.com>
2021-02-22 09:18:38 -0500

    Allow usage with any Github repo, also link contributors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{App, AppSettings, Arg};
-use eyre::{eyre, Result};
+use eyre::{bail, eyre, Result};
 use log::{debug, error, info};
 use once_cell::sync::Lazy;
 use regex::{Captures, Regex};
@@ -7,7 +7,9 @@ use std::fs;
 use std::path::Path;
 use std::time::Duration;
 
-static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r#"[\sa-zA-Z0-9]#([0-9]+)"#).unwrap());
+static RE_ISSUES_PRS: Lazy<Regex> = Lazy::new(|| Regex::new(r#"[\sa-zA-Z0-9]#([0-9]+)"#).unwrap());
+
+static RE_CONTRIBUTORS: Lazy<Regex> = Lazy::new(|| Regex::new(r#"@([a-z0-9_-]+)"#).unwrap());
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -17,7 +19,7 @@ fn main() -> Result<()> {
         .version(clap::crate_version!())
         .setting(AppSettings::ArgRequiredElseHelp)
         .author("Praveen Perera <praveen@avencera.com>")
-        .about("\nAutomatically link CHANGELOG.MD PRs and Issues")
+        .about("\nAutomatically link CHANGELOG.MD PRs, Issues and Contributors")
         .arg(
             Arg::with_name("file")
                 .value_name("PATH")
@@ -26,27 +28,57 @@ fn main() -> Result<()> {
                 .required(true)
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("repo")
+                .long("repo")
+                .takes_value(true)
+                .required(true)
+                .help("The github repo of the project ex: avencera/rustywind"),
+        )
         .get_matches();
+
+    let repo = matches
+        .value_of("repo")
+        .unwrap()
+        .trim_end()
+        .trim_end_matches("/");
+
+    if !reqwest::blocking::get(&format!("https://github.com/{}", repo))
+        .unwrap()
+        .status()
+        .is_success()
+    {
+        bail!("Invalid GitHub repo please try again with a valid one")
+    }
 
     let file_path = Path::new(matches.value_of("file").expect("Invalid PATH provided")).to_owned();
 
     let contents = fs::read_to_string(&file_path)?;
 
-    let new_contents = RE
-        .replace_all(&contents, |caps: &Captures| {
-            match get_url(format!(
-                "https://github.com/clux/kube-rs/issues/{}",
-                caps[1].to_string()
-            )) {
-                Ok(url) => {
-                    debug!("Added link to PR/ISSUE: {}", &url);
-                    format!(" [#{}]({})", caps[1].to_string(), url)
-                }
-                Err(e) => {
-                    error!("{:?}", e);
-                    caps[0].to_string()
-                }
+    let new_contents = RE_ISSUES_PRS.replace_all(&contents, |caps: &Captures| {
+        match get_url(format!(
+            "https://github.com/{}/issues/{}",
+            &repo,
+            caps[1].to_string()
+        )) {
+            Ok(url) => {
+                debug!("Added link to PR/ISSUE: {}", &url);
+                format!(" [#{}]({})", caps[1].to_string(), url)
             }
+            Err(e) => {
+                error!("{:?}", e);
+                caps[0].to_string()
+            }
+        }
+    });
+
+    let new_contents = RE_CONTRIBUTORS
+        .replace_all(&new_contents, |caps: &Captures| {
+            format!(
+                "[{}](https://github.com/{})",
+                caps[0].to_string(),
+                caps[1].to_string()
+            )
         })
         .to_string();
 
@@ -55,7 +87,7 @@ fn main() -> Result<()> {
 
 fn get_url(try_url: String) -> Result<String> {
     let mut retries = 1;
-    let mut res = reqwest::blocking::get(&try_url).unwrap();
+    let mut res = reqwest::blocking::get(&try_url)?;
 
     while !res.status().is_success() && retries <= 15 {
         info!(
@@ -65,7 +97,7 @@ fn get_url(try_url: String) -> Result<String> {
             retries
         );
         std::thread::sleep(Duration::from_millis(2750 + (250 * retries)));
-        res = reqwest::blocking::get(&try_url).unwrap();
+        res = reqwest::blocking::get(&try_url)?;
         retries = retries + 1;
     }
 
